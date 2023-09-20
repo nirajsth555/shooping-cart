a7adb3e78f62d37a287bf6e58d889274635ba360
niraj.shrestha <niraj.shrestha@amniltech.com>
2023-09-20 16:15:25 +0545

    adds navigation in header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,16 +1,19 @@
+import { PUBLIC_ROUTES } from "../constants/routes";
 import { useGetCartProducts } from "../hooks/query/useCart";
 import CartImage from "./image/CartImage";
+import { useNavigate } from "react-router-dom";
 
 export default function Header() {
     const { data } = useGetCartProducts();
+    const navigate = useNavigate();
     return (
         <div className="nav">
             <div className="container">
                 <div className="navbar">
                     <div className="logo">
-                        <h3>Daraz</h3>
+                        <h3 onClick={() => navigate(PUBLIC_ROUTES.HOME)}>Daraz</h3>
                     </div>
-                    <div className="store">
+                    <div className="store" onClick={() => navigate(PUBLIC_ROUTES.CART)}>
                         <CartImage />
                         <div className="count">{data?.length || 0}</div>
                     </div>
